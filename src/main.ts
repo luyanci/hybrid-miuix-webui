3f1e5541833485125e204303196ca4d57692419a
YC酱luyancib <luyancib@qq.com>
2026-06-11 23:31:06 +0800

    add Vconsole

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { createApp } from 'vue'
+import Vconsole from 'vconsole'
 import i18n, { initI18n } from './locales'
 import './style.css'
 import 'miuix-vue/style.css'
@@ -6,7 +7,7 @@ import App from './App.vue'
 
 const app = createApp(App)
 app.use(i18n)
-
+// new Vconsole() // unless need to debug,dont uncomment it
 const init = async () => {
   const savedLocale = localStorage.getItem('locale')
   await initI18n(savedLocale ?? undefined)
